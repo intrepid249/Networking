b070d1f55c3e431c83ed053a8fc2dd811a6e1716
Jack <jack.w.mccall@gmail.com>
2018-03-26 08:09:31 +1100

    Added prototype for application UI and started creating the library

---
+++ Server_Client_CSHARP/App_Prototype/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace App_Prototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNewNode = new System.Windows.Forms.Button();
+            this.btnEditNode = new System.Windows.Forms.Button();
+            this.nodeGraph1 = new TextAdventureNodes.NodeGraph();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtServerStatus = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnEditNode);
+            this.groupBox1.Controls.Add(this.btnNewNode);
+            this.groupBox1.Location = new System.Drawing.Point(13, 423);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(174, 71);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Node";
+            // 
+            // btnNewNode
+            // 
+            this.btnNewNode.Location = new System.Drawing.Point(7, 22);
+            this.btnNewNode.Name = "btnNewNode";
+            this.btnNewNode.Size = new System.Drawing.Size(75, 35);
+            this.btnNewNode.TabIndex = 0;
+            this.btnNewNode.Text = "New";
+            this.btnNewNode.UseVisualStyleBackColor = true;
+            // 
+            // btnEditNode
+            // 
+            this.btnEditNode.Location = new System.Drawing.Point(89, 22);
+            this.btnEditNode.Name = "btnEditNode";
+            this.btnEditNode.Size = new System.Drawing.Size(75, 35);
+            this.btnEditNode.TabIndex = 1;
+            this.btnEditNode.Text = "Edit";
+            this.btnEditNode.UseVisualStyleBackColor = true;
+            // 
+            // nodeGraph1
+            // 
+            this.nodeGraph1.Location = new System.Drawing.Point(13, 13);
+            this.nodeGraph1.Name = "nodeGraph1";
+            this.nodeGraph1.Size = new System.Drawing.Size(961, 404);
+            this.nodeGraph1.TabIndex = 2;
+            this.nodeGraph1.Text = "nodeGraph1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtServerStatus);
+            this.groupBox2.Controls.Add(this.btnDisconnect);
+            this.groupBox2.Controls.Add(this.btnConnect);
+            this.groupBox2.Controls.Add(this.txtPort);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtIP);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(194, 424);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(780, 70);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Server";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(34, 21);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(113, 22);
+            this.txtIP.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(194, 22);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(78, 22);
+            this.txtPort.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(278, 15);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 41);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(359, 15);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(101, 41);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // txtServerStatus
+            // 
+            this.txtServerStatus.Location = new System.Drawing.Point(468, 22);
+            this.txtServerStatus.Name = "txtServerStatus";
+            this.txtServerStatus.Size = new System.Drawing.Size(306, 22);
+            this.txtServerStatus.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 506);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.nodeGraph1);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Text Adventure";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEditNode;
+        private System.Windows.Forms.Button btnNewNode;
+        private TextAdventureNodes.NodeGraph nodeGraph1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServerStatus;
+        private System.Windows.Forms.Button btnDisconnect;
+    }
+}
+
---
+++ Server_Client_CSHARP/TextAdventureNodes/NodeGraph.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace TextAdventureNodes
+{
+    // http://etodd.io/2014/05/16/the-poor-mans-dialogue-tree/
+    // https://www.youtube.com/watch?v=e-VZFC2TLCU
+    public partial class NodeGraph : Control
+    {
+        List<Node> nodes = new List<Node>();
+
+        public NodeGraph()
+        {
+            InitializeComponent();
+        }
+
+        public void AddNode()
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            pe.Graphics.FillRectangle(new HatchBrush(HatchStyle.LargeGrid, Color.Gray, Color.DarkGray), ClientRectangle);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+
+        }
+    }
+}
